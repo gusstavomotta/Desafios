7f0ecc609cdd0ec4439ac1c9eea16b74796cd94b
Gustavo Oliveira da Motta <127642611+gusstavomotta@users.noreply.github.com>
2024-01-21 09:04:53 -0300

    atualizado domingo

--- functions.php
+++ functions.php
@@ -2,20 +2,23 @@
 
 function multiplica_matriz_quadrada(array $matrizX, array $matrizY): array
 {
-    for ($i = 0; $i <= (sizeof($matrizX) - 1); $i++) {
-        for ($j = 0; $j <= (sizeof($matrizX) - 1); $j++) {
-            $matriz_resultado[$i][$j] = ($matrizX[$i][$j] * $matrizY[$i][$j]);
+    $matriz_resultado = null;
+    //primeiro loop percorre as linhas da matriz
+    for ($i = 0; $i < (sizeof($matrizX)); $i++) {
+        //segundo loop percorre as colunas
+        for ($j = 0; $j < (sizeof($matrizY[0])); $j++) {
+            //terceiro loop percorre as colunas verticalmente
+            for ($x = 0; $x < (sizeof($matrizX[0])); $x++) {
+                $matriz_resultado[$i][$j] += ($matrizX[$i][$x] * $matrizY[$x][$j]);
+            }
         }
     }
+    //retorna a matriz resultado
     return $matriz_resultado;
 }
 
 function multiplica_matriz_normal(array $matrizX, array $matrizY) //: array
 {
-
-    //$matriz_resultado[][] = null;
-    //return $matriz_resultado;
-
 }
 
 function printa_matriz($matriz)
--- index.php
+++ index.php
@@ -2,19 +2,26 @@
 require "./functions.php";
 
 
-$matriz_quadrada_1 = [[22, 44, 3], [17, 18, 5], [4, 6, 7]];
-$matriz_quadrada_2 = [[2, 3, 4], [5, 6, 7], [4, 5, 8]];
+$matriz_quadrada_1 = [
+    [2, 3],
+    [5, 6]
+];
+$matriz_quadrada_2 = [
+    [3, 4],
+    [6, 7]
+];
+
 
 $matriz_quadrada_resultado = multiplica_matriz_quadrada($matriz_quadrada_1, $matriz_quadrada_2);
 echo "\nMatriz Quadrada: \n";
 printa_matriz($matriz_quadrada_resultado);
 
 /*
+
 $matriz_normal_1 = [[1, 2, 3], [2, 4, 5]];
 $matriz_normal_2 = [[5, 7, 8], [3, 6, 7]];
 
 $matriz_normal_resultado = multiplica_matriz_normal($matriz_normal_1, $matriz_normal_2);
-echo "\nMatriz Normal: \n"
+echo "\nMatriz Normal: \n";
 printa_matriz($matriz_normal_resultado);
-
- */
+*/
