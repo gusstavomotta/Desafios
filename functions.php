f2ea3d8fcea41a9f0315af2984e89693d429be61
Gustavo Oliveira da Motta <127642611+gusstavomotta@users.noreply.github.com>
2024-01-21 10:00:41 -0300

    Atualizado 10:00

--- functions.php
+++ functions.php
@@ -2,23 +2,41 @@
 
 function multiplica_matriz_quadrada(array $matrizX, array $matrizY): array
 {
-    $matriz_resultado = null;
-    //primeiro loop percorre as linhas da matriz
+    $matriz_resultado = array();
     for ($i = 0; $i < (sizeof($matrizX)); $i++) {
-        //segundo loop percorre as colunas
         for ($j = 0; $j < (sizeof($matrizY[0])); $j++) {
-            //terceiro loop percorre as colunas verticalmente
             for ($x = 0; $x < (sizeof($matrizX[0])); $x++) {
                 $matriz_resultado[$i][$j] += ($matrizX[$i][$x] * $matrizY[$x][$j]);
             }
         }
     }
-    //retorna a matriz resultado
     return $matriz_resultado;
 }
 
-function multiplica_matriz_normal(array $matrizX, array $matrizY) //: array
+function multiplica_matriz_normal(array $matriz_A, array $matriz_B): array
 {
+    $linhas_A = sizeof($matriz_A);
+    $colunas_A = sizeof($matriz_A[0]);
+
+    $linhas_B = sizeof($matriz_B);
+    $colunas_B = sizeof($matriz_B[0]);
+
+    if ($colunas_A != $linhas_B) {
+        echo "Impossível realizar a operação! O número de colunas da primeira matriz difere do número de linhas da segunda matriz.";
+        exit;
+    }
+
+    $matriz_resultado = array_fill(0, $linhas_A, array_fill(0, $colunas_B, 0));
+
+    for ($i = 0; $i < $linhas_A; $i++) {
+        for ($j = 0; $j < $colunas_B; $j++) {
+            for ($x = 0; $x < $colunas_A; $x++) {
+                $matriz_resultado[$i][$j] += ($matriz_A[$i][$x] * $matriz_B[$x][$j]);
+            }
+        }
+    }
+
+    return $matriz_resultado;
 }
 
 function printa_matriz($matriz)
--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 require "./functions.php";
 
-
 $matriz_quadrada_1 = [
     [2, 3],
     [5, 6]
@@ -11,17 +10,22 @@ $matriz_quadrada_2 = [
     [6, 7]
 ];
 
-
 $matriz_quadrada_resultado = multiplica_matriz_quadrada($matriz_quadrada_1, $matriz_quadrada_2);
 echo "\nMatriz Quadrada: \n";
 printa_matriz($matriz_quadrada_resultado);
 
-/*
 
-$matriz_normal_1 = [[1, 2, 3], [2, 4, 5]];
-$matriz_normal_2 = [[5, 7, 8], [3, 6, 7]];
+
+$matriz_normal_1 = [
+    [1, 2, 3],
+    [2, 4, 5]
+];
+$matriz_normal_2 = [
+    [5, 7],
+    [3, 6],
+    [2, 3],
+];
 
 $matriz_normal_resultado = multiplica_matriz_normal($matriz_normal_1, $matriz_normal_2);
 echo "\nMatriz Normal: \n";
 printa_matriz($matriz_normal_resultado);
-*/
