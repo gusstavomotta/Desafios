df53a9af661c53532ce64626b064e100cf11638e
Gustavo Oliveira da Motta <gumotta2908@hotmail.com>
2024-01-22 22:08:13 -0300

    Update index.php

--- desafio2/index.php
+++ desafio2/index.php
@@ -4,17 +4,16 @@ $limite = 15;
 $pagina = isset($_GET['page']) ? (int)$_GET['page'] : 1;
 $inicio = ($limite * $pagina) - $limite;
 
-$url = "https://pokeapi.co/api/v2/pokemon?$limite=150&offset=$inicio";
-
+$url = "https://pokeapi.co/api/v2/pokemon?limit=150&offset=0";
 if (!file_exists('pokemons.txt')) {
     $pokemons = json_decode(file_get_contents($url));
     file_put_contents('pokemons.txt', json_encode($pokemons->results));
 }
 
 $array_pokemons = json_decode(file_get_contents('pokemons.txt'), true);
-$array_pokemons = array_slice($array_pokemons, $inicio, $limite);
-?>
+$array_pokemons_paginado = array_slice($array_pokemons, $inicio, $limite);
 
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -28,12 +27,12 @@ $array_pokemons = array_slice($array_pokemons, $inicio, $limite);
     <h1>Lista dos pokemons</h1>
 
     <ul>
-        <?php foreach ($array_pokemons as $pokemon) : ?>
+        <?php foreach ($array_pokemons_paginado as $pokemon) : ?>
             <li><?php echo $pokemon['name']; ?></li>
         <?php endforeach; ?>
     </ul>
-    <div class="paginamentamento">
-        <?php for ($i = 1; $i <= ceil(150 / $limite); $i++) : ?>
+    <div class="paginacao">
+        <?php for ($i = 1; $i <= ceil(sizeof($array_pokemons) / $limite); $i++) : ?>
             <a href="?page=<?php echo $i; ?>"><?php echo $i; ?></a>
         <?php endfor; ?>
     </div>
